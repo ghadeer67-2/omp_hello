f648e43dd66bbf715b63298f016243356589a974
ghadeer67-2 <149381714+ghadeer67-2@users.noreply.github.com>
2023-10-30 14:27:39 +0300

    Update parallel_for_hello.c

--- parallel_for_hello.c
+++ parallel_for_hello.c
@@ -2,19 +2,22 @@
 #include <stdlib.h>
 #include <omp.h>
 
-
+void Hello (void);
 
 int main(int argc, char* argv[]) {	
 
 	int thread_count = strtol(argv[1], NULL, 10);
 	
 # pragma omp parallel for num_threads(thread_count)	
-  
-	for(int i= 1; i <= omp_get_num_threads(); i++){ //i is private in each thread
-		int my_thread = omp_get_thread_num();
-		
-		printf("Hello from thread %d\n", my_thread);
-	}
-	
+  Hello();
 	return 0;
 }
+void Hello(void) {
+	int my_rank = omp_get_thread_num ();
+	int thread_count = omp_get_num_threads ();
+		
+		printf("Hello from thread %d of \n", my_rank, thread_count );
+	} 
+	
+	
+
